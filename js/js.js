3fd21448c4e826065037b96d5c254dd5776b1ced
NikitkaZ <89261332+NikitkaZ@users.noreply.github.com>
2021-08-24 10:25:27 +0400

    new FreeHelp

---
+++ js/js.js
@@ -0,0 +1,33 @@
+document.getElementById('form').addEventListener("submit", checkForm)
+
+
+function checkForm(event) {
+    event.preventDefault();
+    var el = document.getElementById('form');
+
+    var needhelp = el.needhelp.value;
+    var radio = el.radio.value;
+    var needhelpproblem = el.needhelpproblem.value;
+    var fail = "";
+
+    if (needhelp == "" || radio == "" || needhelpproblem == "")
+        fail = "Заполните все поля";
+    
+    if (fail != "") {
+        alert(fail) ;
+
+        return false;
+    }
+    else {
+        
+        window.location = 'FreeHelp.html';
+       
+        return false;
+    }
+    
+}
+
+//document.querySelector('.buttoncontener').addEventListener('click', () => {
+  //          let data = document.querySelector('.needhelpwindow').value;
+      //      document.querySelector('.menu1').innerHTML = data;
+    //    })
